e89cdae2b70623c56ffeb54ca1943b613d0097c4
Kyle <silverwolf115432@yahoo.com.au>
2021-07-28 20:42:00 +1000

    Kyle Dev Scene - Attempting a dialouge system using external notepad entry options.

---
+++ Assets/Scenes/Programmers/Kyle/Scripts/DialogueParser.cs
@@ -0,0 +1,140 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+public class DialogueParser : MonoBehaviour
+{
+    //we are representing a DialogueLine. Each one of our lines has a few components to it. 
+    //Each line has a name, content, pose, position, and a fifth variable which is going to be the options the player will have when given choices.
+    struct DialogueLine
+    {
+        public string name;
+        public string content;
+        public int pose;
+        public string position;
+        public string[] options;
+
+        public DialogueLine(string Name, string Content, int Pose, string Position)
+        {
+            name = Name;
+            content = Content;
+            pose = Pose;
+            position = Position;
+            options = new string[0];
+        }
+    }
+    // a variable to contain all the different lines of dialogue we will have. We are using a List to hold the DialogueLines.
+    List<DialogueLine> lines;
+
+    // Use this for initialization
+    //What this function is doing for us is dynamically getting the dialogue file that we saved by looking at the name of the Unity Scene we are in and getting the number from that.
+    //It also instantiates the lines List which tells the computer to give us memory to store things in the List because we are about to put things in the List.
+    void Start()
+    {
+        string file = "Assets/Data/Dialogue";
+        string sceneNum = EditorApplication.currentScene;
+        sceneNum = Regex.Replace(sceneNum, "[^0-9]", "");
+        file += sceneNum;
+        file += ".txt";
+
+        lines = new List<DialogueLine>();
+
+        LoadDialogue(file);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void LoadDialogue(string filename)
+    {
+        string line;
+        StreamReader r = new StreamReader(filename);
+
+        using (r)
+        {
+            do
+            {
+                line = r.ReadLine();
+                if (line != null)
+                {
+                    string[] lineData = line.Split(';');
+                    if (lineData[0] == "Player")
+                    {
+                        DialogueLine lineEntry = new DialogueLine(lineData[0], "", 0, "");
+                        lineEntry.options = new string[lineData.Length - 1];
+                        for (int i = 1; i < lineData.Length; i++)
+                        {
+                            lineEntry.options[i - 1] = lineData[i];
+                        }
+                        lines.Add(lineEntry);
+                    }
+                    else
+                    {
+                        DialogueLine lineEntry = new DialogueLine(lineData[0], lineData[1], int.Parse(lineData[2]), lineData[3]);
+                        lines.Add(lineEntry);
+                    }
+                }
+            }
+            while (line != null);
+            r.Close();
+        }
+    }
+
+
+    //The following will output the files information to other areas
+
+
+    public string GetPosition(int lineNumber)
+    {
+        if (lineNumber < lines.Count)
+        {
+            return lines[lineNumber].position;
+        }
+        return "";
+    }
+
+    public string GetName(int lineNumber)
+    {
+        if (lineNumber < lines.Count)
+        {
+            return lines[lineNumber].name;
+        }
+        return "";
+    }
+
+    public string GetContent(int lineNumber)
+    {
+        if (lineNumber < lines.Count)
+        {
+            return lines[lineNumber].content;
+        }
+        return "";
+    }
+
+    public int GetPose(int lineNumber)
+    {
+        if (lineNumber < lines.Count)
+        {
+            return lines[lineNumber].pose;
+        }
+        return 0;
+    }
+
+    public string[] GetOptions(int lineNumber)
+    {
+        if (lineNumber < lines.Count)
+        {
+            return lines[lineNumber].options;
+        }
+        return new string[0];
+    }
+}
+
