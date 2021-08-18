391bc02d77c2eab1ff47cfa8802518be2c1a204b
RS-SKM <81538660+RS-SKM@users.noreply.github.com>
2021-08-18 16:34:10 +1000

    Unity Analytic Testing

---
+++ Assets/Scenes/Programmers/Rath/ButtonPressTest.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPressTest : MonoBehaviour
+{
+    public void ButtonIsPressed()
+    {
+        Debug.Log("Button is pressed, send data to Unity Analytic");
+    }
+}
